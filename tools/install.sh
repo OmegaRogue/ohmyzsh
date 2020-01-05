5fa5a13ed5b822f1da7c4b6cdfbebad10e6dd499
OmegaRogue <thatomegarogue@gmail.com>
2020-01-05 15:47:06 +0100

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -33,7 +33,7 @@ set -e
 
 # Default settings
 ZSH=${ZSH:-~/.oh-my-zsh}
-REPO=${REPO:-ohmyzsh/ohmyzsh}
+REPO=${REPO:-OmegaRogue/ohmyzsh}
 REMOTE=${REMOTE:-https://github.com/${REPO}.git}
 BRANCH=${BRANCH:-master}
 
